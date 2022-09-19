3811eb87d8d43d0833f1ad24c47a13b60730f124
JangYunSeo129 <evilharu901@gmail.com>
2022-09-19 14:50:27 +0900

    drill05
    2019180037 장윤서

---
+++ DRILL_05/drill05.py
@@ -0,0 +1,33 @@
+import turtle
+
+def up_move():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def left_move():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def down_move():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def right_move():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+
+turtle.onkey(up_move, 'W')
+turtle.onkey(left_move, 'A')
+turtle.onkey(down_move, 'S')
+turtle.onkey(right_move, 'D')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
